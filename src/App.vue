f5d2e5884bc45b314eb8a8074d79ab705b95eb78
Daniel Svane <danielsvane@gmail.com>
2018-05-28 19:32:07 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app dark>
+    <CreateAccount v-model="showCreateAccountModal"></CreateAccount>
+
+    <v-toolbar fixed app>
+      <v-toolbar-title>Smartplant</v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items>
+        <v-btn flat @click="showCreateAccountModal = true">Create account</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  import CreateAccount from '@/components/CreateAccount'
+
+  export default {
+    components: {
+      CreateAccount
+    },
+    data () {
+      return {
+        showCreateAccountModal: true
+      }
+    }
+  }
+</script>
---
+++ src/components/CreateAccount.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-dialog v-model="show" max-width="500px">
+    <v-card>
+      <v-card-title>
+        Create account
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field v-model="email" label="Email" required></v-text-field>
+          <v-text-field v-model="password" type="password" label="Password" required></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" flat @click="createAccount">Create</v-btn>
+        <v-btn color="primary" flat @click="show=false">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+  export default {
+    props: ['value'],
+    data () {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+      createAccount () {
+
+      }
+    },
+    computed: {
+      show: {
+        get () { return this.value },
+        set (value) { this.$emit('input', value) }
+      }
+    }
+  }
+</script>
